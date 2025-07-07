bdba6d455bf6578d659bef9904da2d706a1f948f
Maram-web <maram.manai@esprit.tn>
2025-07-07 23:24:28 +0200

    ajout DB

--- src/main/java/tn/esprit/vmservice/controller/VmController.java
+++ src/main/java/tn/esprit/vmservice/controller/VmController.java
@@ -1,10 +1,16 @@
 package tn.esprit.vmservice.controller;
 
+import com.sun.jdi.VirtualMachine;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.vmservice.dto.VmRequest;
+import tn.esprit.vmservice.entity.VmInstance;
 import tn.esprit.vmservice.services.VmService;
+import tn.esprit.vmservice.repositories.VmInstanceRepository;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/vm")
@@ -12,6 +18,8 @@ import tn.esprit.vmservice.services.VmService;
 public class VmController {
 
     private final VmService vmService;
+    private final VmInstanceRepository vmInstanceRepository;
+
 
     @PostMapping("/create")
     public ResponseEntity<String> createVm(@RequestBody VmRequest request) {
@@ -23,4 +31,15 @@ public class VmController {
     public String sayHello() {
         return "Hello from vm-service! ✅";
     }
+
+    @GetMapping("/all")
+    public List<VmInstance> getAllVMs() {
+        return vmInstanceRepository.findAll();
+    }
+
+    @GetMapping("/by-user/{username}")
+    public List<VmInstance> getVMsByUser(@PathVariable String username) {
+        return vmInstanceRepository.findByUsername(username);
+    }
+
 }
